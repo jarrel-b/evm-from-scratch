99e502b4f7b303b8d1c9993095f42614aefd0fd0
jarrel <jarrel.biscocho@gmail.com>
2026-06-10 15:41:03 -0700

    Add storage

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,14 @@ const MAXIMUM_STACK_SIZE = 1024;
 class Stack {
   #items: number[] = [];
 
-  public push(value: number): void {
+  push(value: number): void {
     if (this.#items.length === MAXIMUM_STACK_SIZE) {
       throw new Error("stack overflow");
     }
     this.#items.push(value);
   }
 
-  public pop(): number {
+  pop(): number {
     const value = this.#items.pop();
     if (value === undefined) {
       throw new Error("stack underflow");
@@ -18,7 +18,7 @@ class Stack {
     return value;
   }
 
-  public items(): number[] {
+  items(): number[] {
     return [...this.#items];
   }
 }
@@ -26,16 +26,16 @@ class Stack {
 class Memory {
   #bytes = new Uint8Array();
 
-  public access(offset: number, size: number): Uint8Array {
+  access(offset: number, size: number): Uint8Array {
     this.#expand(offset, size);
     return this.#bytes.slice(offset, offset + size);
   };
 
-  public load(offset: number): Uint8Array {
+  load(offset: number): Uint8Array {
     return this.access(offset, 32);
   };
 
-  public store(offset: number, value: Uint8Array): number {
+  store(offset: number, value: Uint8Array): number {
     const gasCost = this.#expand(offset, value.length);
     this.#bytes.set(value, offset);
     return gasCost
@@ -64,6 +64,27 @@ class Memory {
   }
 }
 
+class Storage {
+  #slots = new Map<bigint, bigint>();
+  #accessed = new Set<bigint>();
+
+  load(slot: bigint): [boolean, bigint] {
+    const warm = this.#accessed.has(slot);
+    this.#accessed.add(slot);
+    const val = this.#slots.get(slot);
+    return [warm, val ?? 0n];
+  }
+
+  store(slot: bigint, val: bigint): void {
+    this.#accessed.add(slot);
+    if (val === 0n) {
+      this.#slots.delete(slot);
+      return;
+    }
+    this.#slots.set(slot, val);
+  }
+}
+
 async function main() {
   const stack = new Stack();
   stack.push(2);
@@ -75,6 +96,12 @@ async function main() {
   const cost = memory.store(0, new Uint8Array([0x01, 0x02, 0x03, 0x04]));
   console.log("cost", cost);
   console.log("memory", memory.load(0));
+
+  const storage = new Storage();
+  storage.store(1n, 420n);
+  console.log(storage.load(1n));
+  console.log(storage.load(1n));
+  console.log(storage.load(1337n));
 }
 
 main().catch(console.error);
